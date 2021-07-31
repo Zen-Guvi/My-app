4097a18557c0d2d97e3464d933f7d44e448e6ef6
kaushik-mula <42288846+kaushik-mula@users.noreply.github.com>
2021-07-31 10:11:57 +0530

    Add files via upload

---
+++ Day3/demo.js
@@ -0,0 +1,4 @@
+console.log("this is test source");
+const img = document.createElement("img");
+img.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6e/Harry_Potter_wordmark.svg/1200px-Harry_Potter_wordmark.svg.png";
+document.body.appendChild(img);
